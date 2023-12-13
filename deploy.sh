3389aca15b1248b90701904494250675ceb756b1
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-12-13 17:47:38 -0500

    update pm2

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ if pm2 pid 461Server > /dev/null 2>&1; then
 fi
 
 # Start your application using PM2 with the updated configuration
-pm2 start ./api/pm2.config.json
+pm2 start pm2.config.json
 
 # Save the PM2 process list to restart on server reboot
 pm2 save
